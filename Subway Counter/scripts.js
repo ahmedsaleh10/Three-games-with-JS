98e31a9c5c72ac465c3621c9b120a2718245904d
Ahmed <ahmed.ovicsaleh@outlook.com>
2023-01-21 16:04:17 +0200

    Subway Counter Task Done

--- Subway Counter/scripts.js
+++ Subway Counter/scripts.js
@@ -1,11 +1,15 @@
-// document.getElementById("count-el").innerText=5;
 let count = 0;
-let count_el = document.getElementById("count-el");
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
 function increment() {
   count++;
-  count_el.innerText = count;
+  countEl.textContent = count;
 }
 
 function save() {
-  console.log(count);
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
